55fdf6b9fa90fc2e07d0a6600e37755d366ef4a4
Elijah Gutman <gutmane7@gmail.com>
2024-11-06 13:23:44 -0500

    creating the controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+  def one_product
+    product = Product.first
+    render json: { name: product.name, price: product.price, image_url: product.image_url, description: product.description }
+  end
+
+  def all_products
+    products = Product.all
+    result = []
+    index = 0
+    while index < products.length
+      result.push({ name: products[index].name, price: products[index].price, image_url: products[index].image_url, description: products[index].description })
+      index += 1
+    end
+    render json: result
+  end
 end
