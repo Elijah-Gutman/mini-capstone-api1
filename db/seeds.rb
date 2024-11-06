67eb0c442447d1f916aba22f4aa7918625c3c952
Elijah Gutman <gutmane7@gmail.com>
2024-11-06 12:54:27 -0500

    adding products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.create(name: "Shoes", price: 120, image_url: "https://media.istockphoto.com/id/1350560575/photo/pair-of-blue-running-sneakers-on-white-background-isolated.jpg?s=612x612&w=0&k=20&c=A3w_a9q3Gz-tWkQL6K00xu7UHdN5LLZefzPDp-wNkSU=", description: "A Pair of shoes")
+
+Product.create(name: "Shirts", price: 60, image_url: "https://t4.ftcdn.net/jpg/07/63/26/85/360_F_763268538_sk7wNf87lh0ioZMnAnLBOBlf1unB2RNi.jpg", description: "A stack of shirts")
+
+Product.create(name: "Ties", price: 200, image_url: "https://t4.ftcdn.net/jpg/09/16/54/31/360_F_916543101_AoWJ2snYsTA8sM3BoMLjniUF89y8N7yr.jpg", description: "A nice tie")
